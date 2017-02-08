ea258d77eecb98c4b5aaac5535de6b743623a0bc
GFV450 <gfv450@gmail.com>
2017-02-07 17:16:49 -0800

    Updated diagonal_difference: Better time complexity, more refined code

--- diagonal_difference.py
+++ diagonal_difference.py
@@ -8,21 +8,22 @@ for a_i in range(n):
     a_t = [int(a_temp) for a_temp in input().strip().split(' ')]
     a.append(a_t)
 
-diagOne = -1
-diagTwo = len(a)
-sumOne = 0
-sumTwo = 0
 
-for x in a:
-    diagOne += 1
-    diagTwo -= 1
+def diagonal_difference(a, n):
+    diagOne = -1
+    diagTwo = n
+    sumOne = 0
+    sumTwo = 0
 
-    for y in x:
-        if(x.index(y) == diagOne):
-            sumOne += y
+    for x in a:
+        diagOne += 1
+        diagTwo -= 1
 
-        if(x.index(y) == diagTwo):
-            sumTwo += y
+        sumOne += x[diagOne]
+        sumTwo += x[diagTwo]
 
-answer = abs(sumOne - sumTwo)
+    return abs(sumOne - sumTwo)
+
+
+answer = diagonal_difference(a, n)
 print(answer)
